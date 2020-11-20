89022d74acc7c80621b7faedfa98ad5d388259af
smallskyzzz <247931368@qq.com>
2020-11-20 11:20:24 +0800

    新增：项目初始化、slot示例

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <slot-example>
+      <template v-slot:pre="{value}">
+        <span>前置{{ value }}</span>
+      </template>
+      <template v-slot:next>
+        <span>后置</span>
+      </template>
+    </slot-example>
+  </div>
+</template>
+
+<script>
+import SlotExample from './components/slotExample'
+
+export default {
+  name: 'app',
+  components: {
+      SlotExample
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/slotExample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="slot-example">
+    <slot name="pre" :value="value"></slot>
+    12
+    <slot name="next"></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "slotExample",
+        data() {
+            return {
+                value: Math.random()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
